4548fc9ca60e3bbbab3bc236883a6fc3d46f17f8
thiagopando <thiagopando06@gmail.com>
2024-04-03 10:52:46 -0300

    Update

--- listas-objetos.js
+++ listas-objetos.js
@@ -5,11 +5,24 @@ const estudante = {
     turma: 'JavaScript',
     bolsista: true,
     telefones: ['5511999999999', '5511999999998'],
+    endereco: [{
+        rua: 'Joseph Climber',
+        numero: '45',
+        complemento: 'apto 43'
+    }]
 }
 
-estudante.endereco = {
-    rua: 'Joseph Climber',
-    numero: '45',
-    complemento: 'apto 43'
-}
 
+estudante.endereco.push({
+    rua: 'Dona Clotilde',
+    numero: '71',
+    complemento: ''
+})
+
+
+// console.log(estudante.endereco);
+// console.log(estudante.endereco[1]);
+
+const listaEnderecosComComplemento = estudante.endereco.filter((endereco) => endereco.complemento);
+
+console.log(listaEnderecosComComplemento);
