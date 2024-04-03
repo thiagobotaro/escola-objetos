4bc4f9a7d3822cfa768ac25d4c2c016b662ce7d1
thiagopando <thiagopando06@gmail.com>
2024-04-03 10:04:36 -0300

    'Novo commit'

---
+++ dados-valores.js
@@ -0,0 +1,10 @@
+const estudante = {
+    nome: 'José Silva',
+    idade: 32,
+    cpf: '11772255331',
+    turma: 'JavaScript',
+    bolsista: true,
+    telefones: ['5511999999999', '5511999999998'],
+}
+
+console.log(estudante.telefones)
