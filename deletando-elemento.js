d9262541d5bbb5bece34abb986d966081f07622f
thiagobotaro <thiagobotaro@hotmail.com>
2024-04-03 10:10:09 -0300

    pelo mac

---
+++ deletando-elemento.js
@@ -0,0 +1,16 @@
+const objPersonagem = {
+    nome: "Gandalf",
+    classe: "mago",
+    nivel: "20",
+    aliado: {
+      nome: "Saruman",
+      classe: "mago"
+    },
+    status: "desaparecido"
+   }
+
+console.log(objPersonagem);
+
+delete objPersonagem.status;
+
+console.log(objPersonagem);
---
+++ meu-objeto.js
@@ -0,0 +1,9 @@
+const audi = {
+    modelo: 'A3 Sedan 2.0',
+    aceleracao: 6.69,
+    motor: 'L4, Diant., Turbo ',
+    carroceria: 'Sedan',
+    potencia: '220cv'
+}
+
+console.log(audi);
